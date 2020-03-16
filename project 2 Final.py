8e8ec2b11f5e3406b7c0960b36e489cd4637b169
Ahsan Mahmood <ahsan110425@hotmail.com>
2020-03-16 16:23:44 +0500

    scraping all java questions and saving it in a csv file from codingbat.com

--- project 2 Final.py
+++ project 2 Final.py
@@ -11,7 +11,7 @@ def read_file():
     file.close()
     return data
 
-cars = {'Topic': [] , 'Question': [], 'Description':[] }
+data = {'Topic': [] , 'Question': [], 'Description':[] }
 t=[]
 q=[]
 d=[]
@@ -47,31 +47,7 @@ for key, value in products.items():
 
 #dict_df = pd.DataFrame({ key:pd.Series(value) for key, value in mydict.items() })
 
-cars.update({'Topic': t, 'Question': q, 'Description': d})
+data.update({'Topic': t, 'Question': q, 'Description': d})
 df = pd.DataFrame({key: pd.Series(value) for key, value in cars.items()})
 export_csv = df.to_csv (r'D:\PYTHON LEARNING LECTURES CODES\SECOND LECTURE CODES\webscraping_data.csv', index = None, header=True)
-
-#    print("\n\n")
-  #for key, value in products.items():
-   # print(key," --> https://codingbat.com"+value)
-#url = "https://codingbat.com" + products['Warmup-1']
-#page1 = requests.get(url,headers={'user-agent':ua.chrome})
-
-#soup1 = BeautifulSoup(page1.content,'html.parser')
-#for link in soup1.find_all("a", href=re.compile("prob")):
-#    product.append(link.get('href'))
-#url1 = "https://codingbat.com"+product[13]
-#print(url1)
-
-#print(len(product))
-
-#page2 = requests.get(url1,headers={'user-agent':ua.chrome})
-
-#soup2 = BeautifulSoup(page2.content,'html.parser')
-#for divs in soup2.find_all('div', attrs = {'class':'minh'}):
-    #if (divs.p.a):
-#    print(divs.p.text)
- #   print(divs.next_sibling.next_element)
-  #  print(divs.next_sibling.next_element.next_sibling.next_element)
-   # print(divs.next_sibling.next_element.next_sibling.next_element.next_sibling.next_element)
 
